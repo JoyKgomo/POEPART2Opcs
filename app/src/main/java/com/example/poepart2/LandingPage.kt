950e233ccd4417d762552192466a7fef6bdbb6d1
Joy <st10370660@imconnect.edu.za>
2025-05-02 19:59:49 +0200

    Updated code, added code to the landing page, added query to the expensesdao, fixed some code in the sign up and view button

--- app/src/main/java/com/example/poepart2/LandingPage.kt
+++ app/src/main/java/com/example/poepart2/LandingPage.kt
@@ -4,16 +4,28 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.poepart2.Data.AppDatabase
+
+import com.example.poepart2.Data.ExpensesDao
 
 class LandingPage : AppCompatActivity(){
+
+    private lateinit var db: AppDatabase
+    private lateinit var ExpensesDao: ExpensesDao
+    private lateinit var expenseList: TextView
+
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_landingpage)
 
+        db = AppDatabase.getDatabase(this)
+        ExpensesDao = db.ExpensesDao()
 
+        expenseList = findViewById(R.id.txtExpense)
 
         val goToBudgetPage = findViewById<Button>(R.id.btnAddBudget)
         goToBudgetPage?.setOnClickListener{
@@ -26,5 +38,14 @@ class LandingPage : AppCompatActivity(){
             val intent = Intent( this,Expenses::class.java)
             startActivity(intent)
         }
+
+
 }
+    private suspend fun updateExpenseList() {
+        val expense = ExpensesDao.getAllExpenses()
+        val list = expense.joinToString("\n") { "${it.id} Category: ${it.categoryItem} \t Description: ${it.description} \t Amount: ${it.amount} " }
+        runOnUiThread {
+            expenseList.text = list
+        }
+    }
 }
